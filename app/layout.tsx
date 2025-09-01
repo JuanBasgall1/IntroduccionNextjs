35cadf410caecb38c6ac7c2dccd4f26c899be604
JuanBasgall1 <juanbasgall1@gmail.com>
2025-09-01 20:45:51 -0300

    Enrutación y algunos chiches

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+//importamos los estilos del global.css dentro de ui
+import './ui/global.css';
+//importamos la letra
+import {montserrat} from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="bg-gray-100 py-4 text-center">
+          <p className="text-sm text-gray-600">
+            &copy; {new Date().getFullYear()} Acme Inc. All rights reserved.
+          </p>
+        </footer>
+      </body>
     </html>
   );
 }
